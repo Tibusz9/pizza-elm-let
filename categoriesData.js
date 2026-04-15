8902bba34c147ecd12774bb76d05b9f146394c7c
bfacskoedit <efackov@gmail.com>
2026-04-15 21:55:01 +0200

    Create categoriesData.js

---
+++ categoriesData.js
@@ -0,0 +1,7 @@
+// Kategóriák adatok
+window.CATEGORIES_DATA = [
+  { id: 1, nev: "apród", ar: 850 },
+  { id: 2, nev: "lovag", ar: 1150 },
+  { id: 3, nev: "főnemes", ar: 950 },
+  { id: 4, nev: "király", ar: 1250 }
+];
