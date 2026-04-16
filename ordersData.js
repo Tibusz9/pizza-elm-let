fc053c96b5e6ce6c05f90b913f9b9237705fb6be
bfacskoedit <efackov@gmail.com>
2026-04-16 23:33:06 +0200

    Create ordersData.js

---
+++ ordersData.js
@@ -0,0 +1,17 @@
+// Rendelések adatok
+window.ORDERS_DATA = [
+  { pizzanev: "Popey", darab: 2, felvetel: "2005.11.12 11:21:00", kiszallitas: "2005.11.12 12:11:00" },
+  { pizzanev: "Kagylós", darab: 1, felvetel: "2005.11.12 11:41:00", kiszallitas: "2005.11.12 12:26:00" },
+  { pizzanev: "Barbecue chicken", darab: 1, felvetel: "2005.11.12 12:38:00", kiszallitas: "2005.11.12 13:02:00" },
+  { pizzanev: "Röfi", darab: 1, felvetel: "2005.11.12 13:18:00", kiszallitas: "2005.11.12 13:58:00" },
+  { pizzanev: "Tündi kedvence", darab: 1, felvetel: "2005.11.12 13:44:00", kiszallitas: "2005.11.12 16:53:00" },
+  { pizzanev: "Hercegnő", darab: 2, felvetel: "2005.11.12 14:10:00", kiszallitas: "2005.11.12 14:57:00" },
+  { pizzanev: "Mixi", darab: 1, felvetel: "2005.11.12 14:20:00", kiszallitas: "2005.11.12 16:25:00" },
+  { pizzanev: "Ráki", darab: 3, felvetel: "2005.11.12 14:51:00", kiszallitas: "2005.11.12 17:06:00" },
+  { pizzanev: "Szőke kapitány", darab: 1, felvetel: "2005.11.12 15:13:00", kiszallitas: "2005.11.12 17:12:00" },
+  { pizzanev: "Kagylós", darab: 1, felvetel: "2005.11.12 15:42:00", kiszallitas: "2005.11.12 16:48:00" },
+  { pizzanev: "Sonkás", darab: 1, felvetel: "2005.11.12 16:31:00", kiszallitas: "2005.11.12 16:53:00" },
+  { pizzanev: "Excellent", darab: 1, felvetel: "2005.11.12 17:01:00", kiszallitas: "2005.11.12 19:41:00" },
+  { pizzanev: "Máj Fair Lady", darab: 2, felvetel: "2005.11.12 17:58:00", kiszallitas: "2005.11.12 20:39:00" },
+  { pizzanev: "Frankfurti", darab: 1, felvetel: "2005.11.12 18:48:00", kiszallitas: "2005.11.12 21:45:00" }
+];
